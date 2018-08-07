0ab24ccbfa847914565eaccde04e58166e964c50
Joe McCallister <macattack1380@gmail.com>
2018-08-06 20:01:36 -0600

    Feat: Add test for Name length and content

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -42,6 +42,12 @@ $(function() {
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
+         it('name defined', function(){
+           for(let feed of allFeeds){
+             expect(feed.name).toBeDefined();
+             expect(feed.name).not.toBe(0);
+           }
+         })
     });
 
 
